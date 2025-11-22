ce5e5fbe748cc1c59e7ab57d7845caa6c926471b
Renato A. Silva <araujo.renato@unemat.br>
2025-11-22 19:12:04 -0400

    remove old database creation script and add new table creation script

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+    <head></head>
+    <body>
+        <form method="POST" action="">
+            <p><input type="text" placeholder="E-mail" name="email"></p>
+            <p><input name="senha" type="password"></p>
+            <p><a href="">Esqueci minha senha</a></p>
+            <input type="button" value="Entrar">
+        </form>
+    </body>
+</html>
